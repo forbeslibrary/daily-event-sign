c1bea5a43931b9b0f98be7b9a77112ab6a674855
bkalish <bkalish@gmail.com>
2017-04-19 14:28:56 -0400

    added settings in script file. added events refresh

--- daily-event-sign.js
+++ daily-event-sign.js
@@ -1,5 +1,26 @@
-var content = document.getElementById('api_today_cid228_iid1448');
+var settings = {
+  "eventsRefresh": 60, // refresh events refresh rate, in seconds
+  "calendarID": "228", // LibCal calendar ID
+  "institutionID": "1448", // LibCal institution ID
+  "categories": [5064, 4979, 4981] // an array of categories IDs from LibCal
+};
+
 var clock = document.getElementById('clock');
+var eventDiv;
+
+/**
+ * Called on script load
+ */
+function init() {
+  eventDiv = document.createElement('div');
+  eventDiv.id = 'api_today_cid' + settings.calendarID + '_iid' + settings.institutionID;
+  eventDiv.className = 'eventList';
+  document.getElementsByTagName('body')[0].appendChild(eventDiv);
+  updateClock();
+  loadEvents();
+  setInterval(updateClock, 1000);
+  setInterval(loadEvents, settings.eventsRefresh * 1000);
+}
 
 /**
  * Set the clock to the current time
@@ -10,19 +31,47 @@ function updateClock() {
 }
 
 /**
+ * Build LibCal URL
+ */
+function eventsURL() {
+  var url = 'https://api3.libcal.com/api_events.php?';
+  url = url + 'iid=' + settings.institutionID;
+  url = url + '&m=today';
+  url = url + '&cid=' + settings.calendarID;
+  url = url + '&d=' + settings.categories.join('-');
+  url = url + '&context=object&format=js';
+  url = url + '&nocache=' + Date.now();
+  return url;
+}
+
+/**
+ * Load events
+ */
+function loadEvents() {
+  script = document.createElement('script');
+  script.type = 'text/javascript';
+  script.async = true;
+  script.onload = function(){
+      // remote script has loaded
+      adapt();
+  };
+  script.src = eventsURL();
+  document.getElementsByTagName('head')[0].appendChild(script);
+}
+
+/**
  * adapt the layout according to how much room today's events take up
  */
 function adapt() {
   // scale events if they are two tall for screen
-  var height = content.offsetHeight;
+  var height = eventDiv.offsetHeight;
   if (height > 768) {
     var scaleFactor = 768.0 / height;
-    content.style['transform-origin'] = "top left";
-    content.style.transform = 'scale(' + scaleFactor + ',' + scaleFactor + ')';
+    eventDiv.style['transform-origin'] = "top left";
+    eventDiv.style.transform = 'scale(' + scaleFactor + ',' + scaleFactor + ')';
   } else {
-    content.style.transform = 'translateY(' + (768 - height) / 3 + 'px)';
+    eventDiv.style.transform = 'translateY(' + (768 - height) / 3 + 'px)';
   }
 }
 
-setInterval(updateClock, 1000);
-adapt();
+init();
